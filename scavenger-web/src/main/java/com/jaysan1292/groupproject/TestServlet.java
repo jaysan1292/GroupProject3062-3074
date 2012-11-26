c360443aa760baee85ede920a8507ab6882e0fec
Peter Le <greenkaze_99@hotmail.com>
2012-11-26 11:28:08 -0500

    Test

--- scavenger-web/src/main/java/com/jaysan1292/groupproject/TestServlet.java
+++ scavenger-web/src/main/java/com/jaysan1292/groupproject/TestServlet.java
@@ -9,17 +9,13 @@ import java.io.IOException;
 public class TestServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // TODO Auto-generated method stub
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         super.doGet(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // TODO Auto-generated method stub
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         super.doPost(request, response);
-        //test
+        //test by peter
     }
 }
