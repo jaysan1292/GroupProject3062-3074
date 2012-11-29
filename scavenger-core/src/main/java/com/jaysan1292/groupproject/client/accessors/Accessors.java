e59824e0ab671fc3d0b622b6924fef4d758da7e1
Jason Recillo <jaysan1292@gmail.com>
2012-11-28 20:21:33 -0500

    Added precondition check to Accessors

--- scavenger-core/src/main/java/com/jaysan1292/groupproject/client/accessors/Accessors.java
+++ scavenger-core/src/main/java/com/jaysan1292/groupproject/client/accessors/Accessors.java
@@ -1,5 +1,9 @@
 package com.jaysan1292.groupproject.client.accessors;
 
+import com.google.common.base.Preconditions;
+
+import java.net.URI;
+
 /**
  * Created with IntelliJ IDEA.
  * Date: 28/11/12
@@ -15,34 +19,46 @@ public class Accessors {
     private static ScavengerHuntAccessor scavengerHunts;
     private static TeamAccessor teams;
 
+    private static boolean _hostSet;
+
     private Accessors() {}
 
+    public static void setHost(URI host) {
+        _hostSet = true;
+    }
+
     public static ChallengeAccessor getChallengeAccessor() {
+        Preconditions.checkState(_hostSet, "Service host was not set!");
         if (challenges == null) challenges = new ChallengeAccessor();
         return challenges;
     }
 
     public static CheckpointAccessor getCheckpointAccessor() {
+        Preconditions.checkState(_hostSet, "Service host was not set!");
         if (checkpoints == null) checkpoints = new CheckpointAccessor();
         return checkpoints;
     }
 
     public static PathAccessor getPathAccessor() {
+        Preconditions.checkState(_hostSet, "Service host was not set!");
         if (paths == null) paths = new PathAccessor();
         return paths;
     }
 
     public static PlayerAccessor getPlayerAccessor() {
+        Preconditions.checkState(_hostSet, "Service host was not set!");
         if (players == null) players = new PlayerAccessor();
         return players;
     }
 
     public static ScavengerHuntAccessor getScavengerHuntAccessor() {
+        Preconditions.checkState(_hostSet, "Service host was not set!");
         if (scavengerHunts == null) scavengerHunts = new ScavengerHuntAccessor();
         return scavengerHunts;
     }
 
     public static TeamAccessor getTeamAccessor() {
+        Preconditions.checkState(_hostSet, "Service host was not set!");
         if (teams == null) teams = new TeamAccessor();
         return teams;
     }
