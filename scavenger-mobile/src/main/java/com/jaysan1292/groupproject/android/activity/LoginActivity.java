61ff313abae35a19d98339406cb66e9f417ed9fa
Jason Recillo <jaysan1292@gmail.com>
2012-12-02 01:20:22 -0500

    Started some Android UI

--- scavenger-mobile/src/main/java/com/jaysan1292/groupproject/android/activity/LoginActivity.java
+++ scavenger-mobile/src/main/java/com/jaysan1292/groupproject/android/activity/LoginActivity.java
@@ -1,13 +1,14 @@
-package com.jaysan1292.groupproject.android;
+package com.jaysan1292.groupproject.android.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.jaysan1292.groupproject.android.R;
 
-public class MyActivity extends Activity {
+public class LoginActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
     }
 }
