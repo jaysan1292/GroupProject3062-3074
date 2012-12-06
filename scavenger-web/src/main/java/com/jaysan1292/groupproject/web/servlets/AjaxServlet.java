3e9c09674dd4f66472a8dd37ac8fb2ed0d5dccdc
Peter Le <greenkaze_99@hotmail.com>
2012-12-06 10:53:12 -0500

    scavenger-web: AjaxServlet

---
+++ scavenger-web/src/main/java/com/jaysan1292/groupproject/web/servlets/AjaxServlet.java
@@ -0,0 +1,24 @@
+package com.jaysan1292.groupproject.web.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kazedayz
+ * Date: 06/12/12
+ * Time: 10:52 AM
+ */
+public class AjaxServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
