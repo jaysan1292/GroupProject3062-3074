a0ae59e80a3a3f0d8c1d8a5a163229269a70e079
Jason Recillo <jaysan1292@gmail.com>
2012-12-05 23:38:40 -0500

    scavenger-core: Forgot to mark getDescription() as ignorable

--- scavenger-core/src/main/java/com/jaysan1292/groupproject/data/BaseEntity.java
+++ scavenger-core/src/main/java/com/jaysan1292/groupproject/data/BaseEntity.java
@@ -1,11 +1,14 @@
 package com.jaysan1292.groupproject.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /** @author Jason Recillo */
 public abstract class BaseEntity extends JSONSerializable {
     public abstract long getId();
 
     public abstract void setId(long id);
 
+    @JsonIgnore
     public abstract String getDescription();
 
     @Override
